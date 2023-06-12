942ac162f60a66ff1392eb034ed4471e85003021
Laird WT <laird.ts@gmail.com>
2023-06-12 03:44:22 +0100

    updated CompileShader.bat to use relative path

---
+++ CompileShaders.bat
@@ -0,0 +1,3 @@
+.\libs\vulkan_sdk\Bin\glslc.exe shaders\basic_shader.vert -o shaders\basic_shader.vert.spv
+.\libs\vulkan_sdk\Bin\glslc.exe shaders\basic_shader.frag -o shaders\basic_shader.frag.spv
+pause
